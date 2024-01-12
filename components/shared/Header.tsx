091e13a9a97617c883ea5d267c504e3da531b790
Racha OULDBABAALI <rachaould@gmail.com>
2024-01-12 02:06:06 +0100

    Add Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,33 @@
+import { SignedOut, SignedIn, UserButton  } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full border-b flex items-center">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
+          />
+        </Link>
+      </div>
+      <div className="flex w-32 justify-end gap-3">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+        <SignedOut>
+          <Button asChild className="rounded-full" size="lg">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
